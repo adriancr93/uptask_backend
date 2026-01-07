b39a1966b436dcb3cd50d9e4737b866341129156
Adrian Obregon <adriancr93@hotmail.com>
2026-01-06 19:14:45 -0600

    Create Tasks

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,49 @@
 import { Router } from 'express'
+import { body, param } from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController'
+import { handledinputErrors } from '../middleware/validation'
+import { TaskController } from '../controllers/TaskController'
 
 const router = Router()
 
-router.post('/', ProjectController.createProject)
+router.post('/', 
+    body('projectName').notEmpty().withMessage('Project name is required'),
+    body('clientName').notEmpty().withMessage('Client name is required'),
+    body('description').notEmpty().withMessage('Description is required'),
+    handledinputErrors,
+    ProjectController.createProject
+)
+
 router.get('/', ProjectController.getAllProjects)
+
+router.get('/:id', 
+    param('id').isMongoId().withMessage('Invalid project ID'),
+    handledinputErrors,
+    ProjectController.getProjectById
+)
+
+router.put('/:id', 
+    param('id').isMongoId().withMessage('Invalid project ID'),
+    body('projectName').notEmpty().withMessage('Project name is required'),
+    body('clientName').notEmpty().withMessage('Client name is required'),
+    body('description').notEmpty().withMessage('Description is required'),
+    handledinputErrors,
+    ProjectController.updateProject
+)
+
+router.delete('/:id', 
+    param('id').isMongoId().withMessage('Invalid project ID'),
+    handledinputErrors,
+    ProjectController.deleteProject
+)
+
+/** Routes for tasks */
+
+router.post('/:projectId/tasks', 
+    TaskController.createTask
+)
+
+
+
 
 export default router
