3249f36921bd53397a517df7571c8b27fa067851
Adrian Obregon <adriancr93@hotmail.com>
2026-03-09 19:25:29 -0600

    Auth configuration

---
+++ src/models/Auth.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document} from "mongoose"
+
+export interface IUser extends Document {
+    email: string;
+    password: string;
+    name: string;
+    confirmed: boolean;
+}
+
+const userSchema: Schema = new Schema({
+    email: { type: String, lowercase: true, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    confirmed: { type: Boolean, default: false }
+})
+
+const User = mongoose.model<IUser>('User', userSchema)
+
+export default User
